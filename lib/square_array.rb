c7c53d6db8e6453b9eba1517ef00ff15e5a014eb
Sadia Badhon <sadia.islam.badhon@gmail.com>
2019-10-24 07:47:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,6 +3,6 @@ squared_numbers = []
 counter = 0
 def square_array(numbers)
   while counter < array.lenth do
-    squared_numbers << array[counter] ** 2
+    squared_numbers (<< array[counter] ** 2)
     counter +=1
 end
